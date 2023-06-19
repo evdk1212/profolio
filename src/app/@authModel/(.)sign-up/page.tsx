925416f17e5df8afc971c845201e5afafc72098e
DEEPAKKUMAR E <deepak@Deepaks-MacBook-Air.local>
2023-06-19 20:15:46 +0530

    authModel fix3

--- src/app/@authModel/(.)sign-up/page.tsx
+++ src/app/@authModel/(.)sign-up/page.tsx
@@ -7,7 +7,7 @@ const page: FC = () => {
   return (
     <div className='fixed inset-0 bg-white/20 z-10'>
       <div className='container flex items-center h-full max-w-lg mx-auto'>
-        <div className='relative bg-gray-950 w-full h-fit py-20 px-2 rounded-lg'>
+        <div className='relative bg-white w-full h-fit py-20 px-2 rounded-lg'>
           <div className='absolute top-4 right-4'>
             <CloseModel/>
           </div>
