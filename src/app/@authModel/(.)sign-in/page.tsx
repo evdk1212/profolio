f4b2a9c11c8615c9b4f38b921ad41406cf2963a2
DEEPAKKUMAR E <deepak@Deepaks-MacBook-Air.local>
2023-06-19 20:11:46 +0530

    authModel fix

--- src/app/@authModel/(.)sign-in/page.tsx
+++ src/app/@authModel/(.)sign-in/page.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react'
 
 const page: FC = () => {
   return (
-    <div className='fixed inset-0 bg-zinc-900/20 z-10'>
+    <div className='fixed inset-0 bg-white z-10'>
       <div className='container flex items-center h-full max-w-lg mx-auto'>
         <div className='relative bg-gray-950 w-full h-fit py-20 px-2 rounded-lg'>
           <div className='absolute top-4 right-4'>
