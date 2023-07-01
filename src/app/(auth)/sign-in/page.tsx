fa726197f1067fe871fba1c6b04ee5b2dfc59231
DEEPAKKUMAR E <deepak@Deepaks-MacBook-Air.local>
2023-07-01 19:12:57 +0530

    authModel fix2

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,26 @@
 
-import { getAuthSession } from "@/app/lib/auth";
 import { cn } from "@/app/lib/utils";
 import { buttonVariants } from "@/components/shadecnui/button";
 import { SignIn } from "@/components/ui/SignIn";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { FC } from "react";
 
 
-const page:FC =async ({})=>{
-    const session = await getAuthSession();
-    return <>{session?.user?<div className="h-screen flex flex-col mx-auto my-auto justify-center items-center"><Link href={'/dashboard'}  className={cn(buttonVariants({variant:'outline'}))}>Dashboard</Link></div>:<div className="absolute inset-0">
-    <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
-        <Link href={'/'} className={cn(buttonVariants({variant:'ghost'}),'self-start -mt-20')}><ChevronLeft className="mr-2 h-4 w-4"/> Home</Link>
-        <SignIn/>
-    </div>
-</div>}</> 
+import React, { FC } from 'react'
+
+const page:FC =({})=>{
+    // const session = await getAuthSession();
+//     return <>{session?.user?<div className="h-screen flex flex-col mx-auto my-auto justify-center items-center"><Link href={'/dashboard'}  className={cn(buttonVariants({variant:'outline'}))}>Dashboard</Link></div>:<div className="absolute inset-0">
+//     <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+//         <Link href={'/'} className={cn(buttonVariants({variant:'ghost'}),'self-start -mt-20')}><ChevronLeft className="mr-2 h-4 w-4"/> Home</Link>
+//         <SignIn/>
+//     </div>
+// </div>}</> 
+return <div className="absolute inset-0">
+<div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+    <Link href={'/'} className={cn(buttonVariants({variant:'ghost'}),'self-start -mt-20')}><ChevronLeft className="mr-2 h-4 w-4"/> Home</Link>
+    <SignIn/>
+</div>
+</div>
 }
 export default page
